64c1b5fd59a018fe9e46af2100d31d57de916ef2
Mikhail Alexeev <mishalxv35@gmail.com>
2023-12-14 23:08:54 -0800

    fixing keys

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -6,23 +6,24 @@ import auth from "@react-native-firebase/auth";
 import { GoogleSignin } from "@react-native-google-signin/google-signin";
 import { Button } from "react-native-paper";
 
-GoogleSignin.configure({
-  webClientId:
-    "755490275970-3uaac0dck30bvpi9f6n5uu380gvjcsdt.apps.googleusercontent.com",
-});
+
 const Login = () => {
+  GoogleSignin.configure({
+    webClientId:"755490275970-3uaac0dck30bvpi9f6n5uu380gvjcsdt.apps.googleusercontent.com",
+  });
+  
   async function onGoogleButtonPress() {
     // Check if your device supports Google Play
     await GoogleSignin.hasPlayServices({ showPlayServicesUpdateDialog: true });
     // Get the users ID token
     const { idToken } = await GoogleSignin.signIn();
-
     // Create a Google credential with the token
     const googleCredential = auth.GoogleAuthProvider.credential(idToken);
-
     // Sign-in the user with the credential
-    return auth().signInWithCredential(googleCredential);
+    const user = auth().signInWithCredential(googleCredential);
+    user.then((user) => console.log(user)).catch(console.log);
   }
+  
   return (
     <SafeAreaView>
       <View>
