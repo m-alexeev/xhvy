bee2cc2d5f9f1f8bc4e5fa3f05c80ebc8c21f2eb
Mikhail Alexeev <mishalxv35@gmail.com>
2023-12-17 15:36:25 -0800

    exercise types

--- src/types/exercises.ts
+++ src/types/exercises.ts
@@ -1,28 +1,75 @@
-type MuscleGroupType =
-  | "Chest"
-  | "Back"
-  | "Legs"
-  | "Arms"
-  | "Shoulders"
-  | "Abs"
-  | "Other";
+export type Equipment =
+  | "barbell"
+  | "dumbbells"
+  | "machine"
+  | "cable"
+  | "resistanceband"
+  | "trapbar"
+  | "smithmachine"
+  | "sled"
+  | "yoke"
+  | "farmershandles"
+  | "sandbag"
+  | "bodyweight"
+  | "misc";
 
-type EquipmentType =
-  | "Dumbbells"
-  | "Barbell"
-  | "Kettlebell"
-  | "Resistance bands"
-  | "Medicine ball"
-  | "Weight machine"
-  | "Bodyweight"
-  | "Other";
+export type Force = "push" | "pull" | "push and pull";
+export type Movement = "dynamic" | "isometric";
+export type ExerciseType = "compound" | "isolation";
+export type Muscle =
+  | "triceps"
+  | "biceps"
+  | "pronators"
+  | "supinators"
+  | "forearmflexors"
+  | "forearmextensors"
+  | "brachioradialis"
+  | "quadriceps"
+  | "hamstrings"
+  | "calves"
+  | "adductors"
+  | "abductors"
+  | "glutealmuscles"
+  | "trapezius"
+  | "erectorspinae"
+  | "latissimusdorsi"
+  | "upperchest"
+  | "lowerchest"
+  | "pectoralisminor"
+  | "anteriordeltoid"
+  | "lateraldeltoid"
+  | "posteriordeltoid"
+  | "obliques"
+  | "transverseabdominis"
+  | "rectusabdominis";
 
-interface IExercise {
-  id: number;
+export type MajorMuscleGroup =
+  | "back"
+  | "chest"
+  | "shoulders"
+  | "arms"
+  | "legs"
+  | "core";
+
+export type Tag =
+  | "powerlifting"
+  | "olympic"
+  | "strongman"
+  | "calisthenics"
+  | "plyometric";
+
+export interface IExercise {
+  id: string;
   name: string;
-  muscleGroup: MuscleGroupType;
-  equipment: EquipmentType;
-  description?: string;
+  instrcutions: string;
+  equipment: Equipment;
+  force: Force;
+  type: ExerciseType;
+  tags: Array<Tag>;
+  primaryMuscles:Array<Muscle>;
+  primaryMuscleGroups:Array<MajorMuscleGroup>;
+  secondaryMuscles:Array<Muscle>;
+  secondaryMuscleGroups: Array<MajorMuscleGroup>;
+  movement: Movement;
 }
 
-export { EquipmentType, IExercise, MuscleGroupType };
