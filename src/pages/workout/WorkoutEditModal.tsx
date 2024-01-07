06a49330dc7cbad49be6736d954d2e517f41c92a
Mikhail Alexeev <mishalxv35@gmail.com>
2024-01-06 22:39:11 -0800

    feat: Workout editing screen, visual only, editing not yet working

--- src/pages/workout/WorkoutEditModal.tsx
+++ src/pages/workout/WorkoutEditModal.tsx
@@ -1,9 +1,12 @@
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, TextInput, View } from "react-native";
 import React, { FC } from "react";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { RootStackParamList } from "@app/types/navigation";
 import { getWorkout } from "@app/zustand/hooks";
-import { Text } from "react-native-paper";
+import { Button, Text, useTheme } from "react-native-paper";
+import { ScrollView } from "react-native-gesture-handler";
+import { MD3Colors } from "react-native-paper/lib/typescript/types";
+import WorkoutExerciseCardList from "@app/components/workouts/workoutExercise/WorkoutExerciseCardList";
 
 type EditWorkoutNavigationProps = NativeStackScreenProps<
   RootStackParamList,
@@ -13,16 +16,40 @@ type EditWorkoutNavigationProps = NativeStackScreenProps<
 //NOTE: Try to pass whole object instead of id and fetch
 //and check performance
 const WorkoutEditModal: FC<EditWorkoutNavigationProps> = ({ route }) => {
+  const { colors } = useTheme();
   const { workoutId } = route.params;
   const workout = getWorkout(workoutId);
 
   return (
-    <View>
-      <Text variant="bodyMedium">Editing: {workout?.name}</Text>
+    <View style={{ flex: 1 }}>
+      <ScrollView>
+        <View style={styles(colors).container}>
+          <View style={styles(colors).header}>
+            <TextInput
+              style={styles(colors).workoutTitle}
+              value={workout?.name}
+            />
+            <Text>{workout?.note}</Text>
+            <Text>{workout!.started_at.toLocaleString()}</Text>
+            <Text>{workout!.completed_at!.toLocaleString()}</Text>
+          </View>
+          <WorkoutExerciseCardList exercises={workout!.exercises}/>
+          <Button>Save Changes</Button> 
+          <Button textColor={colors.error}>Cancel</Button> 
+        </View>
+      </ScrollView>
     </View>
   );
 };
 
 export default WorkoutEditModal;
 
-const styles = StyleSheet.create({});
+const styles = (colors: MD3Colors) =>
+  StyleSheet.create({
+    container: { flex: 1, margin: 10 },
+    header: { marginBottom: 10 },
+    workoutTitle: {
+      fontSize: 24,
+      color: colors.onSurface,
+    },
+  });
