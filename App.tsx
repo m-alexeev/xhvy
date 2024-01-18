f4baedcaef704b5458c3e4f4ef40c665fc9f5dac
Mikhail Alexeev <mishalxv35@gmail.com>
2024-01-18 08:49:16 -0800

    feature: removed old font imports && package

--- App.tsx
+++ App.tsx
@@ -6,19 +6,18 @@ import { CombinedDarkTheme, CombinedDefaultTheme } from "./src/themes/base";
 import { ThemeProvider, useThemeSwitch } from "./src/contexts/ThemeContext";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import {
-  Montserrat_400Regular,
-  Montserrat_500Medium,
+  Poppins_400Regular,
+  Poppins_500Medium,
   useFonts,
-} from "@expo-google-fonts/montserrat";
-
-import {Poppins_400Regular, Poppins_500Medium} from '@expo-google-fonts/poppins'
+} from "@expo-google-fonts/poppins";
 
 const AppWrapper = () => {
   const { isThemeDark } = useThemeSwitch();
   let theme = isThemeDark ? CombinedDarkTheme : CombinedDefaultTheme;
 
   const [fontsLoaded, fontsError] = useFonts({
-    Poppins_400Regular, Poppins_500Medium
+    Poppins_400Regular,
+    Poppins_500Medium,
   });
 
   if (!fontsLoaded || fontsError) return null;
